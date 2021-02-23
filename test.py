6aac25b36ee0ff6d2f87038511d760b82b04b568
WangH <wh1359259@163.com>
2021-02-23 14:00:01 +0800

    First commit

---
+++ test.py
@@ -0,0 +1,24 @@
+# import requests
+# from lxml import etree
+#
+#
+#
+# def test():
+#
+#     html = requests.get("http://www.nhc.gov.cn/xcs/yqtb/list_gzbd.shtml")
+#     html = html.text
+#
+#     return html
+#
+#
+#
+# def run():
+#     html = test()
+#     print(html)
+#
+# if __name__ == '__main__':
+#     run()
+
+
+import pip._internal.pep425tags
+print(pip._internal.pep425tags.get_supported())
