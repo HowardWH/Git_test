0eef03c3484a7e57a2a16a5339eb3ebda7085701
WangH <wh1359259@163.com>
2021-02-23 15:21:27 +0800

    delete annotation

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
-# import requests
-# from lxml import etree
-#
-#
-#
-# def test():
-#
-#     html = requests.get("http://www.nhc.gov.cn/xcs/yqtb/list_gzbd.shtml")
-#     html = html.text
-#
-#     return html
-#
-#
-#
-# def run():
-#     html = test()
-#     print(html)
-#
-# if __name__ == '__main__':
-#     run()
+import requests
+from lxml import etree
+
+
+
+def test():
+
+    html = requests.get("http://www.nhc.gov.cn/xcs/yqtb/list_gzbd.shtml")
+    html = html.text
+
+    return html
+
+
+
+def run():
+    html = test()
+    print(html)
+
+if __name__ == '__main__':
+    run()
 
 
 import time
